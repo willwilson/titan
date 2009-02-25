856352692128533f6e43dec046726912fcee060d
willwilson <w@willwilsondev.com [mailto:w@willwilsondev.com]>
2009-02-25 09:55:22 -0800

    ignore, merge conflicts

--- js/example.js
+++ js/example.js
@@ -1,7 +1,15 @@
 $(function(){
-	$.controller.defaults.url = "server/";
-	$("#test-list span").formatDate("F, jS, Y | g:i a", "date");
+	$.controller.defaults.url = "server/index.php";
+	//$("#ex").formatNumber("number", {dec_point: ",", decimals: 4});
+	//$("#ex2").formatDate("M j, Y", "date");
+	//$("#ex2").formatLink("{name}", "{id}", {title: "{date}"});
+	
 
-	list = $.controller.array("list");
+	list = $.controller.array("list", {paginate: {perPage: 10}});
+
+	$("#ex2").format(function(elem, data){
+		$(elem).text($(list).valueForKey("count"));
+	});
+
 	$("#test-list").template(list);
 });
